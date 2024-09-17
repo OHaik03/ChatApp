b6cf233ef20561cc148601068c1edd7fcbfbc3d0
Haik Oganesyan <152342617+OHaik03@users.noreply.github.com>
2024-09-17 13:17:47 -0700

    Update ChatClient.py

--- ChatClient.py
+++ ChatClient.py
@@ -1,10 +1,15 @@
 import socket
 import threading
 
-HOST = '127.0.0.1'
+HOST = '127.0.0.1' #Haik - I THINK we cant use this IP so we'll need to change it, keeping for now so we can test stuff.
 SERVER_PORT = 5000
 
 if __name__ == '__main__':
     
     #Haik - Created client instance, takes ipv4 and tcp packets
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    try:
+        client.connect((HOST, SERVER_PORT))
+    except:
+        print(f"Unable to connect to host {HOST} and port {SERVER_PORT}. ")
