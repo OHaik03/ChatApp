7e952908e9aa4272c06c52fe0c3fb8ad52c98224
Haik Oganesyan <152342617+OHaik03@users.noreply.github.com>
2024-09-17 12:44:33 -0700

    Update ChatApp.py

--- ChatApp.py
+++ ChatApp.py
@@ -1,8 +1,18 @@
-from socket import *
-from threading import *
+import socket 
+import threading
 
 
+HOST = '127.0.0.1'
+SERVERPORT = 5000
 
 
 if __name__ == '__main__':
-    print("Hello")
+    
+    #Haik - created socket class object for the mainServer. SOCK_DGRAM lets us use tcp datagrams
+    mainServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    try:
+        mainServer.bind((HOST, SERVERPORT))
+    except:   
+        print(f"Unable to bind to host {HOST} and server port {SERVERPORT}.") 
+
