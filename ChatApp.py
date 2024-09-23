e84a7406f85c9c73637690628cd02daec529911a
Haik Oganesyan <152342617+OHaik03@users.noreply.github.com>
2024-09-23 16:29:37 -0700

    Update .DS_Store

--- ChatApp.py
+++ ChatApp.py
@@ -4,24 +4,31 @@ import threading
 HOST = '127.0.0.1' #I THINK we have to change this cause we cant use this IP. but  
 SERVERPORT = 5000
 
+def whileListening():
+    #print("I am in here")
+
+    mainServer.listen(1)#Haik - listens for incoming connections
+    print("I am in here")
+
+    
+    while 1: #Haik - accepts incoming connections 
+        connectionSocket, addr = mainServer.accept()
+        #accepts messages up to 1024 bytes
+        message = mainServer.recv(1024)
+        print("I am in while")
+
 
 if __name__ == '__main__':
     
     #Haik - created socket class object for the mainServer. SOCK_DGRAM lets us use tcp datagrams
-    mainServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    mainServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
     #Haik - after it starts running, it confirms it is set up then says it is unable to set up.
     try:
         mainServer.bind((HOST, SERVERPORT)) #Haik - binds the server to the host and port number. if it doesnt work, it prints the error.
         print("Server is set up!")
-
-        mainServer.listen(1)#Haik - listens for incoming connections
-        
-        while 1: #Haik - accepts incoming connections 
-            connectionSocket, addr = mainServer.accept()
-            #accepts messages up to 1024 bytes
-            message = mainServer.recv(1024)
-            
+        whileListening()
+          
             
     except:   
         print(f"Unable to bind to host {HOST} and server port {SERVERPORT}.") #error message
--- ChatClient.py
+++ ChatClient.py
@@ -6,6 +6,9 @@ import threading
 HOST = '127.0.0.1' #Haik - I THINK we cant use this IP so we'll need to change it, keeping for now so we can test stuff. 
 SERVER_PORT = 5000
 
+def getMessage():
+    print("Hi")
+
 if __name__ == '__main__':
     
     #Haik - Created client instance, takes ipv4 and tcp packets
@@ -21,4 +24,3 @@ if __name__ == '__main__':
     except:
         print(f"Unable to connect to host {HOST} and port {SERVER_PORT}. ")
     
-#HI
