d84027bfc06d195e3dd514774386467c0daa32b8
Haik Oganesyan <152342617+OHaik03@users.noreply.github.com>
2024-09-27 14:43:53 -0700

    Update ChatApp.py

--- ChatApp.py
+++ ChatApp.py
@@ -1,32 +1,62 @@
 import socket 
 import threading
 
-HOST = '127.0.0.1' #I THINK we have to change this cause we cant use this IP. but  
+HOST = ' ' #I THINK we have to change this cause we cant use this IP. but  
 SERVERPORT = 5000
+USERS = 4
+currentUsers = [] #Haik - lists of users with their IP
+
+def messageManager(client, clientIP, message):#Haik - Gets message and sends the message to everyone (work in progress)
+    while 1:        
+        tempMsg = client.recv(2048).decode('utf-8')#receives and decodes the message
+        if message != ' ':
+            finalMsg = clientIP + ': ' + message
+            client.sendall(finalMsg.encode()) #Haik - encodes the message then sends the message to all clients
+            
+        else:
+            print(f"The message from {clientIP}is empty.")
+            
+             
+
+def get_clientIP(client): #Haik - gets client IP adress
+    clientIP = socket.gethostbyname(client)
+    return clientIP
+        
+def get_clientSocket(client):
+   clientSocket = client(socket.gethostbyaddr)
+   return clientSocket
+
+def manageClient():#Haik - puts client into user list with their IP address
+        
+    while 1:
+        currentUsers.append(get_clientIP) 
+
+
+
 
 def whileListening():
     #print("I am in here")
 
-    mainServer.listen(1)#Haik - listens for incoming connections
-    print("I am in here")
+    mainServer.listen(USERS)#Haik - listens for incoming connections
+    print(f"I am in here. connected to {client} {address[0]} {address[1]}")
 
     
     while 1: #Haik - accepts incoming connections 
-        connectionSocket, addr = mainServer.accept()
-        #accepts messages up to 1024 bytes
-        message = mainServer.recv(1024)
+        client, address = mainServer.accept()
         print("I am in while")
+
+        threading.Thread(target = manageClient, args=(client, )).start()
 
 
 if __name__ == '__main__':
     
     #Haik - created socket class object for the mainServer. SOCK_DGRAM lets us use tcp datagrams
-    mainServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    mainServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
     #Haik - after it starts running, it confirms it is set up then says it is unable to set up.
     try:
         mainServer.bind((HOST, SERVERPORT)) #Haik - binds the server to the host and port number. if it doesnt work, it prints the error.
-        print("Server is set up!")
+        print(f"Server is set up!")
         whileListening()
           
             
