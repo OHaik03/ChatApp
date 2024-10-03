b7fef8cf1ef56a05e263a3261927c4032a6ea36e
Jian Verdad <jian.verdad@gmail.com>
2024-10-03 13:59:04 -0700

    Update ChatClient.py
    Added listening and sending threads and some starter code for both

--- ChatClient.py
+++ ChatClient.py
@@ -6,19 +6,20 @@ import threading
 #Haik - I THINK we cant use this IP so we'll need to change it, keeping for now so we can test stuff. 
 #Jian - Changed HOST to '8.8.8.8' to utilize user's IP as parameter
 HOST = '8.8.8.8' 
-SERVER_PORT = 5000
+CLIENT_PORT = 5000
+disconnect = False
 
-def getMessage():
+def getsend():
     print("Hi")
     
 def client_start():
-    
     #Haik - Created client instance, takes ipv4 and tcp packets
     #Jian - Changed socket parameter SOCK_STREAM to SOCK_DGRAM to connect client, had issues connecting earlier
+    global client 
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
     try: #Haik - trys connecting to server
-        client.connect((HOST, SERVER_PORT))
+        client.connect((HOST, CLIENT_PORT))
         print(f"You Connected!")
         
         # Jian (debugging) - test to check your IP
@@ -27,5 +28,37 @@ def client_start():
         
      
     except:
-        print(f"Unable to connect to host {HOST} and port {SERVER_PORT}. ")
-         
+        print(f"Unable to connect to host {HOST} and port {CLIENT_PORT}. ")
+
+
+def listen():
+    while True:
+        global disconnect
+        if disconnect:
+            break
+        try:
+            message = client.recv(1024).decode('ascii')
+        except socket.error:
+            print(f"Error when connecting...")
+            client.close()      #Jian - ToDo: create server event to remove this client from all current connected users
+            break
+            
+        
+    
+def send():
+    while True:
+        if disconnect:
+            break
+        # Jian - asks for input and formats message
+        message = f'Message received from {client.getsockname()[0]}\n'
+        message += f'Sender\'s Port: {CLIENT_PORT}\n'
+        message += f'Message: {input("")}'
+        if message[22 + len(client.getsockname()[0]) + 15 + len(CLIENT_PORT) + 11].startswith('/'):
+            print("this is a command")
+            
+    
+
+listen_thread = threading.Thread(target=listen)
+listen_thread.start()
+send_thread = threading.Thread(target=send)
+send_thread.start()
