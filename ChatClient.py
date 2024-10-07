a7eb24e1bbf9bc4aee4ee5949a6da055def8f5b4
Jian Verdad <jian.verdad@gmail.com>
2024-10-07 15:50:36 -0700

    Reformatted ChatClient
    Variable: client, listen and send threads

--- ChatClient.py
+++ ChatClient.py
@@ -5,22 +5,26 @@ import threading
 #Haik - these have to match the server IP and port in order to connect 
 #Haik - I THINK we cant use this IP so we'll need to change it, keeping for now so we can test stuff. 
 #Jian - Changed HOST to '8.8.8.8' to utilize user's IP as parameter
-HOST = '8.8.8.8' 
+HOST = '8.8.8.8'
 CLIENT_PORT = 5000
 disconnect = False
-
-def getsend():
-    print("Hi")
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
 def client_start():
     #Haik - Created client instance, takes ipv4 and tcp packets
     #Jian - Changed socket parameter SOCK_STREAM to SOCK_DGRAM to connect client, had issues connecting earlier
-    global client 
-    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
     
     try: #Haik - trys connecting to server
         client.connect((HOST, CLIENT_PORT))
         print(f"You Connected!")
+        listen_thread = threading.Thread(target=listen)
+        listen_thread.start()
+        print(f"Listen Thread works")
+        send_thread = threading.Thread(target=send)
+        send_thread.start()
+        print(f"Send Thread works")
+        
         
         # Jian (debugging) - test to check your IP
         print(f"Your IP: " + client.getsockname()[0])
@@ -58,7 +62,3 @@ def send():
             
     
 
-listen_thread = threading.Thread(target=listen)
-listen_thread.start()
-send_thread = threading.Thread(target=send)
-send_thread.start()
--- main.py
+++ main.py
@@ -7,5 +7,5 @@ from ChatClient import client_start
 # Jian - Runs 2 threads on 1 machine for client and server
 client_thread = threading.Thread(target=client_start)
 client_thread.start()
-server_thread = threading.Thread(target=server_start)
-server_thread.start()
+# server_thread = threading.Thread(target=server_start)
+# server_thread.start()
