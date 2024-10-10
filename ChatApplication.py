a0118e2d064efeacd3e0a85758e197d365cd87aa
Haik Oganesyan <152342617+OHaik03@users.noreply.github.com>
2024-10-10 15:04:40 -0700

    Add files via upload

--- ChatApplication.py
+++ ChatApplication.py
@@ -1,18 +1,20 @@
 import socket 
 import sys
 import threading
+import sys
 
 # Jian - Special placeholder IP to tell Server to listen and accept connections from ALL IPs
-HOST = "0.0.0.0"
-PORT = 5001
+HOST = "8.8.8.8"
 USERS = 4
 currentUsers = [] #Haik - lists of users with their IP
 
+
 #Haik - created socket class object for the mainServer. SOCK_DGRAM lets us use tcp datagrams
-# Jian - Changed SOCK_DGRAM to SOCK_STREAM to utilize socket.listen() in whileListening Function
 mainServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Jian - Changed SOCK_DGRAM to SOCK_STREAM to utilize socket.listen() in whileListening Function
 
 def messageManager(client, clientIP, message):#Haik - Gets message and sends the message to everyone (work in progress)
+    print("in message thread")
     while 1:        
         tempMsg = client.recv(2048).decode('utf-8')#receives and decodes the message
         if message != ' ':
@@ -37,7 +39,7 @@ def manageClient(client):#Haik - puts client into user list with their IP addres
     while 1:
         currentUsers.append(get_clientIP) 
         
-    threading.Thread(target = messageManager, args=(client)).start()
+    #threading.Thread(target = messageManager, args=(client)).start()
 
 
 def whileListening():
@@ -46,14 +48,17 @@ def whileListening():
 
     # (Debugging): Check if enters whileListening()
     print(f"Listening...")
-    
+    acceptingThread = threading.Thread(target=accepting())
+    acceptingThread.start()
     # Jian - Currently crashes file, temporarily commented out 
     
+def accepting():
     while 1: #Haik - accepts incoming connections
-        print("I am in while")
+        print("I am accepting")
         client, address = mainServer.accept()
         #Haik - start multi threading to manage the users 
         threading.Thread(target = manageClient, args=(client, )).start()
+        
 
 
 def server_start():    
@@ -63,11 +68,9 @@ def server_start():
         mainServer.bind((HOST, PORT))
         print(f"Server is set up!")
         whileListening()
-          
-            
+        
     except:   
         print(f"Unable to bind to host {HOST} and server port {PORT}.") #error message
-
 
 
 #Haik - these have to match the server IP and port in order to connect 
@@ -80,11 +83,13 @@ client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 def client_start():
     #Haik - Created client instance, takes ipv4 and tcp packets
     #Jian - Changed socket parameter SOCK_STREAM to SOCK_DGRAM to connect client, had issues connecting earlier
-    HOST = "8.8.8.8"
+    #HOST = "8.8.8.8"
 
     try: #Haik - trys connecting to server
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client.connect((HOST, PORT))
-        print(f"You Connected!")
+        print(f"You connected to {HOST} {PORT}!")
+        manageClient(client)
         
         # Jian (debugging) - test to check your IP
         print(f"Your IP: " + client.getsockname()[0])
@@ -134,18 +139,42 @@ def receive(server, message):
         else:
             print("The message is empty")
 
+def takeCommands(): #Haik - method for detecting and processing commands
+    while True:
+        command = input().strip()
+        if command == '/help':
+            help_list()
+
+def help_list():
+    print("Commands available:")
+    print("1. /help - Display the available commands.")
+    print("2. /myip - Display the IP address of this process.")
+    print("3. /myport - Display the port on which this process is listening.")
+    print("4. /connect <destination> <port> - Connect to a remote peer.")
+    print("5. /list - Display a list of active connections.")
+    print("6. /terminate <connection id> - Terminate a connection.")
+    print("7. /send <connection id> <message> - Send a message to a connection.")
+    print("8. /exit - Closes the program.")
 
 if __name__ == "__main__":
     # Check if the port number is provided as a command-line argument
-    # if len(sys.argv) != 2:
-    #     print("Usage: python chat.py <port>")
-    #     # Stop the execution because the port number was not supplied
-    #     sys.exit(1)  
+    if len(sys.argv) != 2:
+         print("Usage: python chat.py <port>")
+         #Stop the execution because the port number was not supplied
+         sys.exit(1)
 
     # Get the port number from the command-line argument
-    # PORT = int(sys.argv[1])
+    PORT = int(sys.argv[1])
     
     server_thread = threading.Thread(target=server_start)
     server_thread.start()
+    
+    #server_start() #Haik - Starts teh server and binds it
+    print("Type /help for a menu of app commands.")
+    #client_start()
+    
+    
     client_thread = threading.Thread(target=client_start)
     client_thread.start()
+    
+    takeCommands()
